e0be743a8bc1d10132f0ce0ebc12400a90113096
Andrew Laack <andrew.laack@imbue.com>
2026-01-28 21:21:45 -0600

    Removing

--- imbue_core/imbue_core/upper_case_str_enum.py
+++
@@ -1,7 +0,0 @@
-from enum import StrEnum
-
-
-class UpperCaseStrEnum(StrEnum):
-    @staticmethod
-    def _generate_next_value_(name: str, start: int, count: int, last_values: list[str]) -> str:
-        return name.upper()
