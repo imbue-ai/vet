456c4fc4940df8c1cae14a9de0b1a4f65fe17b54
Andrew Laack <andrew.laack@imbue.com>
2026-01-31 19:33:41 -0600

    Removed more unused code.

--- imbue_core/imbue_core/itertools.py
+++ imbue_core/imbue_core/itertools.py
@@ -1,17 +1,13 @@
 import contextlib
 import itertools
-from typing import AsyncGenerator
-from typing import Callable
 from typing import Generator
 from typing import Iterable
 from typing import Sequence
 from typing import TypeVar
-from typing import cast
 
 from imbue_core.errors import ImbueError
 
 T = TypeVar("T")
-TV = TypeVar("TV")
 
 
 class ImbueItertoolsValueError(ImbueError, ValueError):
@@ -43,31 +39,10 @@ def first(iterable: Iterable[T]) -> T | None:
     return next(iter(iterable), None)
 
 
-async def iterable_to_async(iterable: Iterable[T]) -> AsyncGenerator[T, None]:
-    for item in iterable:
-        yield item
-
-
-def generate_unique(
-    source: Iterable[T],
-    key: Callable[[T], TV] = cast(Callable[[T], TV], lambda item: item),
-) -> Generator[T, None, None]:
-    unique = set()
-    for item in source:
-        value = key(item)
-        if value in unique:
-            continue
-        yield item
-        unique.add(value)
-
-
-def generate_flattened(iterable: Iterable[Iterable[T]]) -> Generator[T, None, None]:
-    for item in iterable:
-        yield from item
-
-
 # TODO replace with itertools.batched when we can require Python 3.12+
-def generate_chunks(iterable: Iterable[T], chunk_size: int) -> Generator[tuple[T, ...], None, None]:
+def generate_chunks(
+    iterable: Iterable[T], chunk_size: int
+) -> Generator[tuple[T, ...], None, None]:
     """Yield successive n-sized chunks from any iterable"""
     chunk = []
     for item in iterable:
