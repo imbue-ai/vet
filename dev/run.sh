bf2238a151dfcf1796bd5d143e6ab5fce19f107b
Andrew Laack <andrew@laack.co>
2026-02-20 16:42:35 -0600

    Added check for podman (default), and updated run command to use --rm to for ephemerality

--- dev/run.sh
+++ dev/run.sh
@@ -2,13 +2,22 @@
 
 IMAGE_NAME="vet"
 
+if command -v podman &> /dev/null; then
+    RUNTIME="podman"
+elif command -v docker &> /dev/null; then
+    RUNTIME="docker"
+else
+    echo "No containerization program detected. Please install podman (preferred) or docker."
+    exit 1
+fi
+
 if [ "$1" = "claude" ]; then
     IMAGE_NAME="vet-claude"
 fi
 
 [ -f .env ] || { echo '.env file not found, please create one before proceeding'; exit 1; }
 
-docker run -it \
+$RUNTIME run --rm -it \
     --mount type=bind,source="$(pwd)",target=/app \
     --env-file .env \
     "$IMAGE_NAME" bash
